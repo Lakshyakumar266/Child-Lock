2017ff2b75b56ce6c8f8328b88a017ceab95fb7c
Lakshya Kumar <kamrakumar275@gmail.com>
2024-08-04 23:40:49 +0530

    registration, login, intro page designing complete

--- app/src/main/java/com/example/childlock/LoginActivity.java
+++ app/src/main/java/com/example/childlock/LoginActivity.java
@@ -7,17 +7,17 @@ import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_login);
 
-        Button nextPage = findViewById(R.id.nextPage);
-        nextPage.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        Button goSignUpPage = findViewById(R.id.Go_signUp);
+        goSignUpPage.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(intent);
             finish();
         });
--- app/src/main/java/com/example/childlock/SignUpActivity.java
+++ app/src/main/java/com/example/childlock/SignUpActivity.java
@@ -7,17 +7,17 @@ import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_signup);
 
-        Button nextPage = findViewById(R.id.nextPage);
-        nextPage.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        Button goLoginPage = findViewById(R.id.Go_login);
+        goLoginPage.setOnClickListener(view -> {
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
             startActivity(intent);
             finish();
         });
