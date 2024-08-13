ba6086ff57e7e64aaf9c9642bab41f7c11674d1e
Lakshya Kumar <kamrakumar275@gmail.com>
2024-08-13 23:54:13 +0530

    login page form filling 2.11

--- app/src/main/java/com/example/childlock/LoginActivity.java
+++ app/src/main/java/com/example/childlock/LoginActivity.java
@@ -3,9 +3,12 @@ package com.example.childlock;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,11 +18,35 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+//        Go To Signup Page
         Button goSignUpPage = findViewById(R.id.Go_signUp);
         goSignUpPage.setOnClickListener(view -> {
             Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(intent);
             finish();
         });
+
+//        Form Handle
+        EditText nameInput = findViewById(R.id.email);
+        EditText passwordInput = findViewById(R.id.Password);
+        CheckBox rememberMe = findViewById(R.id.rememberMe);
+        Button forgotPasswordButton = findViewById(R.id.forgotPassword);
+        Button loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(view -> {
+            String name = nameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+
+            if (rememberMe.isChecked()) {System.out.println("handle remember me");}
+
+            if (name.equals("admin") && password.equals("admin")) {
+                System.out.println("Login Successful");
+            }
+        });
+
+        forgotPasswordButton.setOnClickListener(view -> {
+            // Handle forgot password
+        });
+
     }
 }
